9f8b317e1d61adba41d4f0570026169441d08518
yorditycoon <yordanosmoges2003@gmail.com>
2025-03-08 13:47:13 +0400

    scrollfix

--- app/CompanyForm.tsx
+++ app/CompanyForm.tsx
@@ -3,13 +3,13 @@ import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, ScrollView,
 import { Link } from "expo-router";
 
 
-const WorkerForm = ({  }) => {
+const CompanyForm = ({  }) => {
     
   
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("+971");
-  const [location, setLocation] = useState("Dubai");
+  const [location, setLocation] = useState("");
   const [password, setPassword] = useState("");
   const [cvFile, setCvFile] = useState(null);
   const [emiratesIdFile, setEmiratesIdFile] = useState(null);
@@ -17,9 +17,10 @@ const WorkerForm = ({  }) => {
 
   
   return (
-    <ScrollView contentContainerStyle={styles.scrollContainer}>
+    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.scrollContainer} showsVerticalScrollIndicator={false} >
       
-
+<Image source={require("..//assets/images/splash-icon.png")} style={styles.profileLogo} />
         <Text style={styles.label}>Full Name</Text>
         <TextInput
           style={styles.input}
@@ -51,9 +52,8 @@ const WorkerForm = ({  }) => {
         <TextInput
           style={styles.input}
           placeholder="Location"
-          secureTextEntry
           value={location}
-          onChangeText={setPassword}
+          onChangeText={setLocation}
         />
 
         <Text style={styles.label}>Password</Text>
@@ -107,39 +107,42 @@ const WorkerForm = ({  }) => {
         </TouchableOpacity>
     
     </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   scrollContainer: {
-    flexGrow: 1,
-  },
+   },
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "flex-start",
     backgroundColor: "#fff",
     padding: 15,
+    
   },
   label: {
     fontSize: 16,
     alignSelf: "flex-start",
-    marginBottom: 3,
+    marginBottom: 2,
   },
   input: {
-    width: "100%",
+    width: 350,
     height: 50,
     borderWidth: 1,
     borderColor: "#ccc",
     borderRadius: 10,
     paddingHorizontal: 10,
     marginBottom: 15,
+
   },
   uploadContainer: {
     flexDirection: "row",
     alignItems: "center",
     marginBottom: 15,
   },
+
   uploadButton: {
     flexDirection: "row",
     alignItems: "center",
@@ -159,6 +162,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
   },
+
   pickerContainer: {
     width: "100%",
     borderWidth: 1,
@@ -167,10 +171,12 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     overflow: "hidden",
   },
+
   picker: {
     width: "100%",
     height: 50,
   },
+
   fileBox: {
     width: 150,
     height: 40,
@@ -179,15 +185,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 10,
   },
+
   buttonText: {
     color: "#fff",
     fontSize: 18,
     fontWeight: "bold",
   },
+
   errorText: {
     color: 'red',
     marginBottom: 5,
   },
+
   button: {
     width: "70%",
     height: 50,
@@ -196,40 +205,21 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 50,
     marginTop: 5,
-  },
-  profileContainer: {
-    alignItems: "center",
-    
-  },
-  profileCircle: {
-    width: 120,
-    height: 120,
-    borderRadius: 60,
-    backgroundColor: "#f0f0f0",
-    justifyContent: "center",
-    alignItems: "center",
-    borderWidth: 2,
-    borderColor: "rgba(19, 65, 105, 1)",
-  },
-  profileImage: {
-    width: 120,
-    height: 120,
-    borderRadius: 60,
-  },
-  profileLogo: {
-    width: 60,
-    height: 60,
-    
-  },
-  profileText: {
-    marginTop: 8,
-    fontSize: 14,
-    color: "rgba(19, 65, 105, 1)",
+    marginLeft: 50,
   },
   
+  profileLogo: {
+    width: 80,
+    height: 80,
+    marginLeft:125,
+    marginTop: 50,  
+  },
+
   privacyText: {
     marginLeft: 5,
+    marginBottom:5,
   },
+
   link: {
     color: "#007BFF",
     textDecorationLine: "underline",
@@ -238,4 +228,4 @@ const styles = StyleSheet.create({
 
 
 
-export default WorkerForm;
+export default CompanyForm;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,8 @@ export default function RootLayout() {
   return <Stack >
     <Stack.Screen name="index" options={{ headerShown: false }} />
     <Stack.Screen name="Privacy" options={{ headerShown: false }} />
+    <Stack.Screen name="CompanyForm" options={{ headerShown: false }} />
+    <Stack.Screen name="WorkerForm" options={{ headerShown: false }} />
     </Stack>
     
 }
